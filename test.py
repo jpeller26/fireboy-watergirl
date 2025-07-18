d720a6c4a659bc568d48b76a6369e4c06d010254
Josie Peller <jpeller26@cgps.org>
2025-07-18 10:43:12 -0400

    something weird is happening

--- test.py
+++ test.py
@@ -2,15 +2,15 @@ from cmu_graphics import *
 
 def onAppStart(app):
     app.base = app.height - 50
-    app.fx = 20
+    app.x = 20
     app.fy = app.height - 50
     
 def redrawAll(app):
     drawRect(0,app.base,app.width,app.height)
-    drawRect(app.fx,app.fy,20,40,align = 'bottom-left',fill='orange')
+    drawRect(app.x,app.fy,20,40,align = 'bottom-left',fill='orange')
     
-#def onKeyPress(key,app):
-   #if key == 'left':
-        #app.fx += 1
+def onKeyPress(key,app):
+   if key == 'a':
+        app.x += 20
     
 runApp()
